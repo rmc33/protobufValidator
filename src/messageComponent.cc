125323f22c97d5ef9998c4d89f05a06d6b3d973d
Ryan Conigliaro <rmconigliaro@gmail.com>
2022-11-15 00:49:46 -0500

    WASM_LINKOPTS working

--- src/messageComponent.cc
+++ src/messageComponent.cc
@@ -6,12 +6,20 @@
 #include "messageComponent.h"
 
 namespace Validator  {
+using google::protobuf::util::JsonStringToMessage;
+using google::protobuf::util::MessageToJsonString;
+using google::protobuf::util::JsonPrintOptions;
 
     std::string MessageComponent::Validate(const std::string &test_msg) {
         tutorial::MyMessageComponent m;
-        google::protobuf::Message* message = dynamic_cast<google::protobuf::Message*>(&m);
-        absl::Status status = google::protobuf::util::JsonStringToMessage(test_msg, message);
-        return absl::StrCat("", status.message());
+        absl::Status status = google::protobuf::util::JsonStringToMessage(test_msg, &m);
+        if (status.ok()) {
+            std::string content;
+            if (MessageToJsonString(m, &content, JsonPrintOptions{}).ok()) {
+                return content;
+            }
+        }
+        return absl::StrCat("error", status.message());
     }
 
 } // namespace Validator
